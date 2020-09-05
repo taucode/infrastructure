74deb7fd44fa71976684a441e39bdbbb360c816d
Andrii Kovalenko <ankovalenko@deloitte.com>
2020-09-05 12:07:54 +0300

    Moving to new Azure pipelines scheme.

--- dev-build-release.bat
+++ dev-build-release.bat
@@ -3,7 +3,7 @@ dotnet restore
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.Infrastructure.Tests\TauCode.Infrastructure.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Infrastructure.Tests\TauCode.Infrastructure.Tests.csproj
+dotnet test -c Debug .\test\TauCode.Infrastructure.Tests\TauCode.Infrastructure.Tests.csproj
+dotnet test -c Release .\test\TauCode.Infrastructure.Tests\TauCode.Infrastructure.Tests.csproj
 
 nuget pack nuget\TauCode.Infrastructure.nuspec
